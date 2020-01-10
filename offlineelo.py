1286f79dd2169fcbae6ee87cd9d45f52283f5b96
btmullin <benjamin.t.mullin@gmail.com>
2020-01-09 21:19:28 -0600

    fixing bug

--- offlineelo.py
+++ offlineelo.py
@@ -1,12 +1,32 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="www.nordicraceanalysis.com",
-  user="db_btmullin",
-  passwd="mysql1sCool!"
-  database="nrat_db"
-)
+# constants
 
-mycursor = mydb.cursor()
+def dbquery(query):
+    mydb = mysql.connector.connect(
+      host="www.nordicraceanalysis.com",
+      user="db_btmullin",
+      passwd="mysql1sCool!",
+      database="nrat_db"
+    )
 
-# 
+    mycursor = mydb.cursor()
+
+    mycursor.execute(query)
+    
+    return mycursor
+    
+
+
+
+if __name__== "__main__":
+
+    # get a list of racers
+    # initialize their score
+    
+    # get a list of races
+    # for each racer
+        # for each racer in the race
+            # compare them to all other racers to get an updated score
+        # udpate all racers scores from the race
+    
