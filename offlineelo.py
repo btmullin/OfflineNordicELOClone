eb0585e89a9db257ae200aac0911613403cd729a
btmullin <benjamin.t.mullin@gmail.com>
2020-01-09 21:31:52 -0600

    fixing bug

--- offlineelo.py
+++ offlineelo.py
@@ -14,7 +14,7 @@ def dbquery(query):
 
     mycursor.execute(query)
     
-    return mycursor
+    return mycursor.fetchall()
     
 
 
@@ -23,6 +23,15 @@ if __name__== "__main__":
 
     # get a list of racers
     # initialize their score
+    
+    # drop the table of scores if it exists
+    dbquery("DROP TABLE IF EXISTS EloScore")
+    
+    # create the table of scores again now empty
+    dbquery("CREATE TABLE EloScore (EloID int AUTO_INCREMENT,
+                                    int RacerID NOT NULL,
+                                    int EventID NOT NULL,
+                                    PRIMARY KEY(EloID)")
     
     # get a list of races
     # for each racer
