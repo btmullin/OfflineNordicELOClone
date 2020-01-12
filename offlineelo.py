f1f526e751c7daef63761fd9ff024e233c51f79d
btmullin <benjamin.t.mullin@gmail.com>
2020-01-11 19:43:43 -0600

    Going to start trying to do some calculations with just elm creek skate races

--- offlineelo.py
+++ offlineelo.py
@@ -61,6 +61,7 @@ if __name__== "__main__":
         for racer in racers:
             racer_pts_query = "SELECT Score FROM EloScore, Event WHERE RacerID={} And Event.EventID=EloScore.EventID ORDER BY Event.EventDate DESC LIMIT 1".format(racer[0])
             score = dbquery(racer_pts_query)
+            print score
             if (isinstance(score, tuple)):
                 # append the score
                 racer_starting_points.append(score[0])
